575c051d51ef901a1413c28816939800a5cea188
Chester Agsamosam <chesteragsamosam@gmail.com>
2023-02-14 15:20:28 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'georgeagsamosam.github.io',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
